9070801da19e5ed447d55a427fe01b8bbd9ab3f5
Ведерников Сергей <swnike@mail.ru>
2025-01-02 12:43:32 +0500

    Добавьте файлы проекта.

---
+++ CalcWithLogger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcWithLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
