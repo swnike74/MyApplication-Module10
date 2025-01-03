09b6220db2b36f6b237daf0792b06727bbac555c
Ведерников Сергей <swnike@mail.ru>
2025-01-03 12:20:03 +0500

    update 1

---
+++ Calculator/Calc1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ICalculator
+    {
+        Int32 Sum(Int32 x, Int32 y);
+    }
+    public class Calc1 : ICalculator
+    {
+        Int32 x, y;
+        public Calc1()
+        {
+        }
+        public Calc1(Int32 x, Int32 y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calc1 calculator = new Calc1();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Calculator of Sum: a + b");
+                    Console.WriteLine("Enter digit a:");
+                    int numberA = Convert.ToInt32(Console.ReadLine());
+                    if (numberA.GetType().ToString() != "System.Int32") throw new FormatException();
+
+                    Console.WriteLine("Enter digit b:");
+                    int numberB = Convert.ToInt32(Console.ReadLine());
+                    if (numberB.GetType().ToString() != "System.Int32") throw new FormatException();
+
+                    Console.WriteLine("Результат:{0}",calculator.Sum(numberA,numberB));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
+            }
         }
     }
 }
